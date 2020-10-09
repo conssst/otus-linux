7c47dea9c83ca9b773bdd0911bf61da4a7877f8c
conssst <conssstantinnn@gmail.com>
2020-10-09 18:57:43 +0300

    update

--- Homework/05. NFS/nfs-server.sh
+++ Homework/05. NFS/nfs-server.sh
@@ -4,17 +4,17 @@ mkdir -p ~root/.ssh
 cp ~vagrant/.ssh/auth* ~root/.ssh
 # устанавливаем необходимые пакеты для работы nfs сервера
 dnf install -y nfs-utils
-# создаём дирректории, которые хотим расшарить
+# создаём каталоги, которые хотим расшарить
 mkdir /srv/nfs
 mkdir /srv/nfs/upload
-# задаём права на дирректорию
+# задаём права на каталог
 chmod -R 777 /srv/nfs/upload
 # включаем поддержку udp для nfs сервера
 sed -i '50i\udp=y' /etc/nfs.conf
 # запускаем nfs сервер и rpc
 systemctl enable --now nfs-server
 systemctl enable --now rpcbind
-# указываем дирректорию и добавляем ip клиента в конфигурацию экспорта
+# указываем каталог, ip клиента и права в конфигурации экспорта
 echo '/srv/nfs 192.168.50.13(rw,sync)' >> /etc/exports
 # чтобы изменения вступили в силу перечитываем конфиг экспорта
 exportfs -r
